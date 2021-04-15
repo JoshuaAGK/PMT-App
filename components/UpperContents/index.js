153d0128f642e74a8c3b0de409c7366328dc9a28
Josh <joshuaagkelley@gmail.com>
2021-04-15 12:39:38 +0100

    Added placeholder currency indicator

--- components/UpperContents/index.js
+++ components/UpperContents/index.js
@@ -2,27 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styles from './styles'
 
-// var dateString = "";
-
-// function updateDateString() {
-//     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//     var date = new Date();
-//     var hours = String(date.getHours()).length < 2 ? "0" + String(date.getHours()) : String(date.getHours());
-//     var minutes = String(date.getMinutes()).length < 2 ? "0" + String(date.getMinutes()) : String(date.getMinutes());
-//     document.getElementById("timeOfDay").innerHTML = hours + ":" + minutes;
-    
-    
-//     var monthNum = date.getMonth()
-//     var year = date.getFullYear();
-    
-//     var day = String(days[date.getDay()]);
-//     var date = date.getDate();
-//     var month = String(months[monthNum]);
-    
-//     dateString = day + ", " + date + "th " + month + " " + year;
-// }
-
 function dateString() {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -58,7 +37,10 @@ const UpperContents = (props) => {
     
 
     return (
-        <View><Text>{dateString()}</Text></View>
+        <View style={styles.container}>
+            <Text style={styles.dateText}>{dateString()}</Text>
+            <View style={styles.rightBox}><Text>$4.20</Text></View>
+        </View>
     )
 };
 
--- components/UpperContents/styles.js
+++ components/UpperContents/styles.js
@@ -1,7 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    container: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    dateText: {
+        color: "#AAA",
+        fontWeight: "600"
+    },
+    rightBox: {
+        borderWidth: 1,
+        borderRadius: 50/4,
+        padding: 5,
+        minWidth: 100,
+        alignItems: "center"
+    }
 })
 
 export default styles;
