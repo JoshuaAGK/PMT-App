ba5735eeb975b199e1aeb0e5fc726e7925eba6b0
Idris Khan <idris.khan303@gmail.com>
2021-04-23 12:35:34 +0100

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmhl4_iDPaAZXXa9kX8Ih86-G3jHCU0OQ",
+  authDomain: "pmt-project-3b78e.firebaseapp.com",
+  databaseURL: "https://pmt-project-3b78e-default-rtdb.firebaseio.com",
+  projectId: "pmt-project-3b78e",
+  storageBucket: "pmt-project-3b78e.appspot.com",
+  messagingSenderId: "124983312526",
+  appId: "1:124983312526:web:fef40f53991deb7b5a09df",
+  measurementId: "G-094EV1ZT5K",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
