acdbcb58385f7589e164c17c82cd349f9902a411
Idris Khan <idris.khan303@gmail.com>
2021-05-12 19:14:58 +0100

    Fixed the static stlying to more static styling to allow more space to view the text of a question in Brain training

--- components/DailyActivity/BrainTraining/index.js
+++ components/DailyActivity/BrainTraining/index.js
@@ -8,97 +8,124 @@ import { incrementBalance } from '../../../src/firebase/firestore/firestoreServi
 import { useDispatch, useSelector } from 'react-redux';
 import { addToBalance } from '../../../src/features/auth/authSlice';
 
-const DICLENGTH = Object.keys(dictionary).length
+const DICLENGTH = Object.keys(dictionary).length;
 
-function shuffleAnswers(answers) { // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    for (let i = answers.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = answers[i];
-        answers[i] = answers[j];
-        answers[j] = temp;
-    }
-    return answers;
+function shuffleAnswers(answers) {
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  for (let i = answers.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = answers[i];
+    answers[i] = answers[j];
+    answers[j] = temp;
+  }
+  return answers;
 }
 
 function BrainTraining(props) {
-    let authSelector = useSelector(state => state.auth);
-    let dispatch = useDispatch();
+  let authSelector = useSelector((state) => state.auth);
+  let dispatch = useDispatch();
 
-    const [index, setIndex] = useState(-1);
-    const [score, setScore] = useState(0);
+  const [index, setIndex] = useState(-1);
+  const [score, setScore] = useState(0);
 
-    function beginGame() {
-        setIndex(index + 1);
+  function beginGame() {
+    setIndex(index + 1);
+  }
+
+  function answerQuestion(answer) {
+    if (index == DICLENGTH) {
+      alert('Game over');
+    } else {
+      let correctAnswer = dictionary[Object.keys(dictionary)[index]]['correct'];
+      let points;
+      if (answer == correctAnswer) {
+        setScore(score + 1);
+        points = Points.QUESTION_CORRECT;
+      } else {
+        points = Points.QUESTION_INCORRECT;
+      }
+      dispatch(addToBalance(points));
+      incrementBalance(authSelector.currentUser.balance, points);
+      setIndex(index + 1);
     }
+  }
 
-    function answerQuestion(answer) {
-        if (index == DICLENGTH) {
-            alert("Game over");
-        } else {
-            let correctAnswer = dictionary[Object.keys(dictionary)[index]]["correct"];
-            let points;
-            if (answer == correctAnswer) {
-                setScore(score + 1);
-                points = Points.QUESTION_CORRECT;
-            } else {
-                points = Points.QUESTION_INCORRECT;
-            }
-            dispatch(addToBalance(points));
-            incrementBalance(authSelector.currentUser.balance, points);
-            setIndex(index + 1);
-        }
-    }
+  if (index == -1) {
+    return (
+      <View style={styles.brainTraining}>
+        <Text style={styles.titleText}>Brain Training</Text>
+        <Pressable
+          style={[mainStyles.platformShadow, styles.beginGameButton]}
+          onPress={() => beginGame()}
+        >
+          <Text style={styles.beginGameText}>Begin game!</Text>
+        </Pressable>
+      </View>
+    );
+  } else if (index == DICLENGTH) {
+    return (
+      <View style={styles.brainTraining}>
+        <Text style={styles.scoreText}>
+          Game over! You scored {score} / {DICLENGTH}
+        </Text>
+      </View>
+    );
+  } else {
+    let word = Object.keys(dictionary)[index];
+    let answers = shuffleAnswers([
+      dictionary[word]['incorrect0'],
+      dictionary[word]['incorrect1'],
+      dictionary[word]['incorrect2'],
+      dictionary[word]['correct'],
+    ]);
 
-    if (index == -1) {
-        return (
-          <View style={styles.brainTraining}>
-            <Text style={styles.titleText}>Brain Training</Text>
-            <Pressable style={[mainStyles.platformShadow, styles.beginGameButton]} onPress={() => beginGame()}>
-              <Text style={styles.beginGameText}>Begin game!</Text>
+    let answer1 = answers[0];
+    let answer2 = answers[1];
+    let answer3 = answers[2];
+    let answer4 = answers[3];
+
+    return (
+      <View style={styles.brainTraining}>
+        <Text style={styles.indexText}>
+          Question: {index + 1} / {DICLENGTH}{' '}
+        </Text>
+        <Text style={styles.scoreText}>Score: {score}</Text>
+
+        <View style={styles.interaction}>
+          <Text style={styles.questionWord}>
+            What is the meaning of &quot;{word}&quot;?
+          </Text>
+
+          <View style={styles.buttons}>
+            <Pressable
+              style={styles.button}
+              onPress={() => answerQuestion(answer1)}
+            >
+              <Text style={styles.buttonText}>{answer1}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button}
+              onPress={() => answerQuestion(answer2)}
+            >
+              <Text style={styles.buttonText}>{answer2}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button}
+              onPress={() => answerQuestion(answer3)}
+            >
+              <Text style={styles.buttonText}>{answer3}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.button}
+              onPress={() => answerQuestion(answer4)}
+            >
+              <Text style={styles.buttonText}>{answer4}</Text>
             </Pressable>
           </View>
-        )
-      } else if (index == DICLENGTH) {
-        return (
-          <View style={styles.brainTraining}>
-            <Text style={styles.scoreText}>Game over! You scored {score} / {DICLENGTH}</Text>
-          </View>
-        )
-      } else {
-        let word = Object.keys(dictionary)[index];
-        let answers = shuffleAnswers([dictionary[word]["incorrect0"], dictionary[word]["incorrect1"], dictionary[word]["incorrect2"], dictionary[word]["correct"]]);
-
-        let answer1 = answers[0];
-        let answer2 = answers[1];
-        let answer3 = answers[2];
-        let answer4 = answers[3];
-
-        return (
-            <View style={styles.brainTraining}>
-            <Text style={styles.indexText}>Question: {index + 1} / {DICLENGTH} </Text>
-            <Text style={styles.scoreText}>Score: {score}</Text>
-    
-            <View style={styles.interaction}>
-                <Text style={styles.questionWord}>What is the meaning of "{word}"?</Text>
-    
-                <View style={styles.buttons}> 
-                    <Pressable style={styles.button} onPress={() => answerQuestion(answer1)}>
-                        <Text style={styles.buttonText}>{answer1}</Text>
-                    </Pressable>
-                    <Pressable style={styles.button} onPress={() => answerQuestion(answer2)}>
-                        <Text style={styles.buttonText}>{answer2}</Text>
-                    </Pressable>
-                    <Pressable style={styles.button} onPress={() => answerQuestion(answer3)}>
-                        <Text style={styles.buttonText}>{answer3}</Text>
-                    </Pressable>
-                    <Pressable style={styles.button} onPress={() => answerQuestion(answer4)}>
-                        <Text style={styles.buttonText}>{answer4}</Text>
-                    </Pressable>
-                </View>
-            </View>
-            </View>
-        )
-      }
+        </View>
+      </View>
+    );
+  }
 }
 
 export default BrainTraining;
--- components/DailyActivity/styles.js
+++ components/DailyActivity/styles.js
@@ -3,14 +3,14 @@ import { StyleSheet, Dimensions } from 'react-native';
 const scaleFontSize = (fontSize) => {
   const ratio = fontSize / 375;
   const newSize = Math.round(ratio * Dimensions.get('window').width);
-  return newSize; 
+  return newSize;
 };
 
 const styles = StyleSheet.create({
   activityContainer: {
-    height: 160,
-    width: "100%"
-  }
+    height: 400,
+    width: '100%',
+  },
 });
 
 export default styles;
